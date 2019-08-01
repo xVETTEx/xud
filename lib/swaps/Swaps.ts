dc3d4f4f97bccd3c716243d188911ceee6b09c69
Karl Ranna <karl@karlranna.com>
2019-08-01 20:08:10 +0000

    fix(swaps): token address comparison

--- lib/swaps/Swaps.ts
+++ lib/swaps/Swaps.ts
@@ -733,7 +733,7 @@ class Swaps extends EventEmitter {
         return false;
     }
 
-    if (tokenAddress !== expectedTokenAddress) {
+    if (tokenAddress && tokenAddress.toLowerCase() !== expectedTokenAddress!.toLowerCase()) {
       this.logger.error(`received token address ${tokenAddress}, expected ${expectedTokenAddress}`);
       this.failDeal(deal, SwapFailureReason.InvalidResolveRequest, `Token address ${tokenAddress} did not match ${expectedTokenAddress}`);
       return false;
