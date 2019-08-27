ca97a08e62072194ddb40119b69d511ebac10bd6
Karl Ranna <karl@karlranna.com>
2019-08-27 10:48:35 +0000

    use test:sim

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -xe
 declare -xp
-export GOPATH="$PWD/go"
-export CI=false
+GOPATH="$PWD/go"
 GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
 GETH_PATH="$GOPATH/src/github.com/ethereum/go-ethereum"
 if [ ! -d "$GETH_PATH" ]; then
   git clone $GETH_SOURCE "$GETH_PATH"
   cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
-  go run build/ci.go install ./cmd/geth
+  CI=false GO111MODULE=off go run build/ci.go install ./cmd/geth
 fi
 echo "Geth installed $("$GETH_PATH"/build/bin/geth --version)"
