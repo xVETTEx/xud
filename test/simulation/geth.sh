56564e9a399edca3dbb3afd18b19a7b314790d38
Karl Ranna <karl@karlranna.com>
2019-08-27 09:51:26 +0000

    do not fetch if exists

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -4,6 +4,9 @@ declare -xp
 export GOPATH="$PWD/go"
 export CI=false
 GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
-git clone $GETH_SOURCE "$GOPATH/src/github.com/ethereum/go-ethereum"
-cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
-go run build/ci.go install ./cmd/geth
+if [ ! -d "$DIRECTORY" ]; then
+  git clone $GETH_SOURCE "$GOPATH/src/github.com/ethereum/go-ethereum"
+  cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
+  go run build/ci.go install ./cmd/geth
+fi
+echo "Geth installed $($GETH_SOURCE/build/bin/geth --version)"
