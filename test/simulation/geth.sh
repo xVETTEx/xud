7c1ba2ba7af67ed667c2e624097c5c206abd1b30
Karl Ranna <karl@karlranna.com>
2019-08-27 10:38:34 +0000

    fixup geth source

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -5,7 +5,7 @@ export GOPATH="$PWD/go"
 export CI=false
 GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
 GETH_PATH="$GOPATH/src/github.com/ethereum/go-ethereum"
-if [ ! -d "$GETH_SOURCE" ]; then
+if [ ! -d "$GETH_PATH" ]; then
   git clone $GETH_SOURCE "$GETH_PATH"
   cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
   go run build/ci.go install ./cmd/geth
