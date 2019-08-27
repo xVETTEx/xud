ec7cdc5482aba4ce80a9baa585214f41f31f9d3e
Karl Ranna <karl@karlranna.com>
2019-08-27 09:15:15 +0000

    enable gomodules

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -3,10 +3,10 @@ set -xe
 declare -xp
 eval "$(GIMME_GO_VERSION=1.12 gimme)"
 export GOPATH="$PWD/go"
-# export CI=false
+export CI=false
 rm -rf "$PWD/go"
 rm -rf "$PWD/temp"
 git clone https://github.com/ethereum/go-ethereum "$GOPATH/src/github.com/ethereum/go-ethereum"
 cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
 # git checkout v1.9.2
-go run build/ci.go install ./cmd/geth
+GO111MODULE=on go run build/ci.go install ./cmd/geth
