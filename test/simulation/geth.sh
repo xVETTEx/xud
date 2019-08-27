aaf1cca2a443e4cfd59e2b81a4cfa944b28a85a7
Karl Ranna <karl@karlranna.com>
2019-08-27 08:51:37 +0000

    temp: geth

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -xe
 declare -xp
-# export PATH="$PWD/go/bin:$PATH"
-# export PATH="$PWD/go/bin:$PATH"
-# export GOPATH="$PWD/go"
-# GETH_PATH="$PWD/go/src/github.com/ethereum/go-ethereum"
-# git clone --verbose https://github.com/ethereum/go-ethereum "$GETH_PATH"
+eval "$(GIMME_GO_VERSION=1.12 gimme)"
+export GOPATH="/home/travis/builds/xud/test/simulation/go"
+rm -rf "$PWD/go"
+rm -rf "$PWD/temp"
 git clone https://github.com/ethereum/go-ethereum "$GOPATH/src/github.com/ethereum/go-ethereum"
 cd "$GOPATH/src/github.com/ethereum/go-ethereum" || exit 1
 git checkout v1.9.2
-# cd "$GETH_PATH" || exit 1
-# echo "go version $(go version)"
-# go run build/ci.go install
 go run build/ci.go install ./cmd/geth
