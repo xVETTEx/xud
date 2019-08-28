522b61b7e87392f04f599cfa550652425839c1a1
Karl Ranna <karl@karlranna.com>
2019-08-28 19:17:00 +0000

    disk space

--- test/simulation/install.sh
+++ test/simulation/install.sh
@@ -2,47 +2,48 @@
 set -xe
 
 export GO_PATH=$PWD/go
-CACHE_PATH="$PWD/cache"
-
-GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
-GETH_PATH="$GO_PATH/src/github.com/ethereum/go-ethereum"
-GETH_COMMIT_HASH="e0bb1631c21042336d230c11de0dfe8580aa28c4"
-./install-geth.sh "$GETH_SOURCE" "$GETH_PATH" "$GETH_COMMIT_HASH"
-
-SOLC_SOURCE="https://github.com/ethereum/solidity/releases/download/v0.4.23/solidity-ubuntu-trusty.zip"
-SOLC_PATH="$CACHE_PATH/solc"
-SOLC_SHA256SUM="1006dc09dc46f396641931b0494383b8d73b3fd2b447f5300f5d2dbe5fd23368"
-./install-solc.sh "$SOLC_SOURCE" "$SOLC_PATH" "$SOLC_SHA256SUM"
-export PATH="$SOLC_PATH:$PATH"
-echo "solc version: $(solc --version)"
-
-RAIDEN_CONTRACTS_REPOSITORY="https://github.com/ExchangeUnion/raiden-contracts.git"
-RAIDEN_CONTRACTS_BRANCH="simnet-contracts"
-RAIDEN_CONTRACTS_PATH="$CACHE_PATH/raiden-contracts"
-RAIDEN_CONTRACTS_COMMIT_HASH="8a705ba98da0b6ab1a53282a9ac330930850201a"
-./install-raiden-contracts.sh \
-  "$RAIDEN_CONTRACTS_REPOSITORY" \
-  "$RAIDEN_CONTRACTS_BRANCH" \
-  "$RAIDEN_CONTRACTS_PATH" \
-  "$RAIDEN_CONTRACTS_COMMIT_HASH"
-
-AUTOMINER_VENV_DIR="autominer-venv"
-AUTOMINER_SOURCE="$PWD/utils/autominer"
-./install-autominer.sh "$CACHE_PATH" "$AUTOMINER_VENV_DIR" "$AUTOMINER_SOURCE"
-
-GETH_DATA_DIR="$PWD/geth"
-# GETH_DATA_DIR="$CACHE_PATH/geth"
-GETH_BINARY_PATH="$GETH_PATH/build/bin/geth"
-# GENESIS_JSON="$PWD/utils/genesis.json"
-# ./create-geth-genesis.sh "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GENESIS_JSON"
-
-GETH_NETWORK_ID=4321
-GETH_RPCADDR="localhost"
-GETH_PORT=8545
-./start-geth.sh "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GETH_NETWORK_ID" "$GETH_RPCADDR" "$GETH_PORT"
-
-TREASURY_ACCOUNT_PATH="$GETH_DATA_DIR/keystore/treasury"
-./create-ethereum-account.sh "$TREASURY_ACCOUNT_PATH" "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GETH_NETWORK_ID"
-
-# debug
-sleep 60
+echo "free disk space $(df -h)"
+# CACHE_PATH="$PWD/cache"
+#
+# GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
+# GETH_PATH="$GO_PATH/src/github.com/ethereum/go-ethereum"
+# GETH_COMMIT_HASH="e0bb1631c21042336d230c11de0dfe8580aa28c4"
+# ./install-geth.sh "$GETH_SOURCE" "$GETH_PATH" "$GETH_COMMIT_HASH"
+#
+# SOLC_SOURCE="https://github.com/ethereum/solidity/releases/download/v0.4.23/solidity-ubuntu-trusty.zip"
+# SOLC_PATH="$CACHE_PATH/solc"
+# SOLC_SHA256SUM="1006dc09dc46f396641931b0494383b8d73b3fd2b447f5300f5d2dbe5fd23368"
+# ./install-solc.sh "$SOLC_SOURCE" "$SOLC_PATH" "$SOLC_SHA256SUM"
+# export PATH="$SOLC_PATH:$PATH"
+# echo "solc version: $(solc --version)"
+#
+# RAIDEN_CONTRACTS_REPOSITORY="https://github.com/ExchangeUnion/raiden-contracts.git"
+# RAIDEN_CONTRACTS_BRANCH="simnet-contracts"
+# RAIDEN_CONTRACTS_PATH="$CACHE_PATH/raiden-contracts"
+# RAIDEN_CONTRACTS_COMMIT_HASH="8a705ba98da0b6ab1a53282a9ac330930850201a"
+# ./install-raiden-contracts.sh \
+  # "$RAIDEN_CONTRACTS_REPOSITORY" \
+  # "$RAIDEN_CONTRACTS_BRANCH" \
+  # "$RAIDEN_CONTRACTS_PATH" \
+  # "$RAIDEN_CONTRACTS_COMMIT_HASH"
+#
+# AUTOMINER_VENV_DIR="autominer-venv"
+# AUTOMINER_SOURCE="$PWD/utils/autominer"
+# ./install-autominer.sh "$CACHE_PATH" "$AUTOMINER_VENV_DIR" "$AUTOMINER_SOURCE"
+#
+# GETH_DATA_DIR="$PWD/geth"
+# # GETH_DATA_DIR="$CACHE_PATH/geth"
+# GETH_BINARY_PATH="$GETH_PATH/build/bin/geth"
+# # GENESIS_JSON="$PWD/utils/genesis.json"
+# # ./create-geth-genesis.sh "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GENESIS_JSON"
+#
+# GETH_NETWORK_ID=4321
+# GETH_RPCADDR="localhost"
+# GETH_PORT=8545
+# ./start-geth.sh "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GETH_NETWORK_ID" "$GETH_RPCADDR" "$GETH_PORT"
+#
+# TREASURY_ACCOUNT_PATH="$GETH_DATA_DIR/keystore/treasury"
+# ./create-ethereum-account.sh "$TREASURY_ACCOUNT_PATH" "$GETH_BINARY_PATH" "$GETH_DATA_DIR" "$GETH_NETWORK_ID"
+#
+# # debug
+# sleep 60
