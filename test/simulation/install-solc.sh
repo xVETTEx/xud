7ff532b90038d25d2681a8fe6b5429f75f3807d0
Karl Ranna <karl@karlranna.com>
2019-08-28 08:30:09 +0000

    install solc

---
+++ test/simulation/install-solc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+# SOLC_SOURCE=$1
+SOLC_SOURCE=$1
+SOLC_PATH=$2
+EXPECTED_SHA256SUM=$3
+install_solc () {
+  echo "installing solc"
+  mkdir -p "$SOLC_PATH"
+  cd "$SOLC_PATH"
+  wget "$SOLC_SOURCE"
+  unzip ./*.zip
+  export PATH="$SOLC_PATH:$PATH"
+}
+if [ ! -d "$SOLC_PATH" ]; then
+  install_solc
+else
+  ROOT=$PWD
+  cd "$SOLC_PATH"
+  CURRENT_SHA256SUM=$(sha256sum solc | awk '{print $1}')
+  if [ "$CURRENT_SHA256SUM" == "$EXPECTED_SHA256SUM" ]; then
+    echo "solc already installed"
+  else
+    echo "updating solc"
+    cd "$ROOT"
+    rm -Rf "$SOLC_PATH"
+    install_solc
+  fi
+fi
--- test/simulation/install.sh
+++ test/simulation/install.sh
@@ -2,7 +2,12 @@
 set -xe
 
 export GO_PATH=$PWD/go
-GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
-GETH_PATH="$GO_PATH/src/github.com/ethereum/go-ethereum"
-GETH_COMMIT_HASH="e0bb1631c21042336d230c11de0dfe8580aa28c4"
-./install-geth.sh "$GETH_SOURCE" "$GETH_PATH" "$GETH_COMMIT_HASH"
+# GETH_SOURCE="https://github.com/ExchangeUnion/go-ethereum"
+# GETH_PATH="$GO_PATH/src/github.com/ethereum/go-ethereum"
+# GETH_COMMIT_HASH="e0bb1631c21042336d230c11de0dfe8580aa28c4"
+# ./install-geth.sh "$GETH_SOURCE" "$GETH_PATH" "$GETH_COMMIT_HASH"
+
+SOLC_SOURCE="https://github.com/ethereum/solidity/releases/download/v0.4.23/solidity-ubuntu-trusty.zip"
+SOLC_PATH="$PWD/cache/solc"
+SOLC_SHA256SUM="1006dc09dc46f396641931b0494383b8d73b3fd2b447f5300f5d2dbe5fd23368"
+./install-solc.sh "$SOLC_SOURCE" "$SOLC_PATH" "$SOLC_SHA256SUM"
