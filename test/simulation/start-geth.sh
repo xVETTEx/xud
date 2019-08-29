7e24083d4f90e62c31253b8bcfc5bc44a9de3470
Karl Ranna <karl@karlranna.com>
2019-08-29 07:13:17 +0000

    ethash dir

--- test/simulation/start-geth.sh
+++ test/simulation/start-geth.sh
@@ -5,4 +5,4 @@ GETH_DATA_DIR=$2
 GETH_NETWORK_ID=$3
 GETH_RPCADDR=$4
 GETH_PORT=$5
-GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --rpcapi "eth,net,web3,txpool" --rpc --rpcaddr "$GETH_RPCADDR" --rpcport "$GETH_PORT" --nodiscover --maxpeers 0 &
+GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --rpcapi "eth,net,web3,txpool" --rpc --rpcaddr "$GETH_RPCADDR" --rpcport "$GETH_PORT" --nodiscover --maxpeers 0 --ethash.dagdir "$GETH_DATA_DIR/.ethash" &
