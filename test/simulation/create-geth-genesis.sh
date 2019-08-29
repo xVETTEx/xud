c1d16e82fe93efbaf351be61ebc3da3d7f57df8c
Karl Ranna <karl@karlranna.com>
2019-08-29 14:45:34 +0000

    deploy contracts

--- test/simulation/create-geth-genesis.sh
+++ test/simulation/create-geth-genesis.sh
@@ -5,7 +5,6 @@ GETH_DATA_DIR=$2
 GENESIS_JSON=$3
 if [ -d "$GETH_DATA_DIR" ]; then
   rm -Rf "$GETH_DATA_DIR"
-  echo "Geth data already exists"
-else
-  GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" init "$GENESIS_JSON"
+  echo "Deleting geth data"
 fi
+GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" init "$GENESIS_JSON"
