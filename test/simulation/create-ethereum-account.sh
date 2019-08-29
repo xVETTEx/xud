f7e045ab4294c792b685a193797b938488d5cb20
Karl Ranna <karl@karlranna.com>
2019-08-29 15:43:45 +0000

    wait for DAG

--- test/simulation/create-ethereum-account.sh
+++ test/simulation/create-ethereum-account.sh
@@ -4,8 +4,6 @@ NEW_ACCOUNT_PATH=$1
 GETH_BINARY_PATH=$2
 GETH_DATA_DIR=$3
 GETH_NETWORK_ID=$4
-# TODO: More reliable way of waiting for geth to boot
-sleep 5
 CREATED_ACCOUNT=$($GETH_BINARY_PATH --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --exec "loadScript(\"$PWD/utils/create-account.js\")" attach | grep "account:" | awk '{print $2}')
 echo "created account is: $CREATED_ACCOUNT"
 mv $GETH_DATA_DIR/keystore/*$CREATED_ACCOUNT "$NEW_ACCOUNT_PATH"
