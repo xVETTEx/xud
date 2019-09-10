ad93009ec14843e286140706291e39e28ab7e240
Karl Ranna <karl@karlranna.com>
2019-09-10 09:22:24 +0000

    better geth-start

--- test/simulation/start-geth.sh
+++ test/simulation/start-geth.sh
@@ -5,17 +5,13 @@ if [ ! -d "$DAG_DIR" ]; then
   echo "DAG dir does not exist"
   WAIT_FOR_DAG=true
 fi
-GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --rpcapi "eth,net,web3,txpool" --rpc --rpcaddr "$GETH_RPCADDR" --rpcport "$GETH_PORT" --nodiscover --maxpeers 0 --etherbase=0x0000000000000000000000000000000000000000 --ethash.dagdir "$DAG_DIR" >> "$TEMP_PATH/geth.log" 2>&1&
-# wait for geth to boot
+GOPATH="$GO_PATH" "$GETH_BINARY_PATH" --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --rpcapi "eth,net,web3,txpool" --rpc --rpcaddr "$GETH_RPCADDR" --rpcport "$GETH_PORT" --nodiscover --maxpeers 0 --etherbase=0x778c58e06fb57d93411b1c329c64cdfc5d984da4 --ethash.dagdir "$DAG_DIR" >> "$TEMP_PATH/geth.log" 2>&1&
+# TODO: check geth.ipc -> geth has been booted
 sleep 5
+# TODO: add etherebase to params
+./generate-ethereum-blocks.sh 100
 if [ $WAIT_FOR_DAG ]; then
-  # TODO: use ./generate-ethereum-blocks.sh
-  $GETH_BINARY_PATH --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --exec "var acc = personal.newAccount(''); miner.start(500)" attach
   echo "waiting for DAG"
-  EXPECTED_CI_DAG_GENERATION_TIME=540
+  EXPECTED_CI_DAG_GENERATION_TIME=240
   sleep $EXPECTED_CI_DAG_GENERATION_TIME
-else
-  # TODO: use ./generate-ethereum-blocks.sh
-  $GETH_BINARY_PATH --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --exec "var acc = personal.newAccount(''); miner.start(100)" attach
-  sleep 1
 fi
