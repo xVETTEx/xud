9660baa84c6ab7436360fdf7f62fc32f7166b6ec
Karl Ranna <karl@karlranna.com>
2019-09-10 18:30:03 +0000

    cleanup block gen

--- test/simulation/create-ethereum-account.sh
+++ test/simulation/create-ethereum-account.sh
@@ -4,5 +4,4 @@ source .env
 
 NEW_ACCOUNT_PATH=$1
 CREATED_ACCOUNT=$($GETH_BINARY_PATH --datadir "$GETH_DATA_DIR" --networkid "$GETH_NETWORK_ID" --exec "loadScript(\"$PWD/utils/create-account.js\")" attach | grep "account:" | awk '{print $2}')
-echo "created account is: $CREATED_ACCOUNT"
 mv $GETH_DATA_DIR/keystore/*$CREATED_ACCOUNT "$NEW_ACCOUNT_PATH"
--- test/simulation/utils/create-account.js
+++ test/simulation/utils/create-account.js
@@ -1,6 +1,3 @@
 var password = "";
 var account = personal.newAccount(password);
-miner.setEtherbase(account)
-// TODO: generate funds for the account later?
-miner.start(500);
 console.log("account:", account.slice(2));
