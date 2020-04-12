47fa931129e8e32995f3131f61ffe4a694b32e87
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-04-12 17:53:06 +0300

    Create deposit_withdraw.ts

---
+++ lib/ccxt/deposit_withdraw.ts
@@ -0,0 +1,6 @@
+function fetchDepositAddresses(){
+  //jotenki pitäis saada ehkä noden btc- ja erc-osoite? Sit node avais uudet payment channelit kun tohon osoitteeseen tulee varoja.
+}
+
+function withdraw(currency, amount, address){
+}
