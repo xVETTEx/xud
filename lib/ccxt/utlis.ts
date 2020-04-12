ede5e3a1859cef07a5fdd13274b4f43d7e6a8db7
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-04-12 17:16:35 +0300

    Create utlis.ts

---
+++ lib/ccxt/utlis.ts
@@ -0,0 +1,17 @@
+//onkohan utils sopiva nimi tälle folderille?
+
+function getTimestamp(){
+}
+
+function getDaytime(){
+}
+
+function getSymbol(pair string){
+  //eli tää niinku tradinpair, mut ccxt:ssä kutsutaan symboliksi. ja uppercase lettereillä. BTC/LTC.
+}
+
+function getBase(pair string){
+}
+
+function getQuote(pair string){
+}
