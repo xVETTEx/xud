b6f2352933019fc55e086e27672d5fdfd937e9a6
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-05-01 11:56:18 +0300

    Create localId.ts

---
+++ lib/orderbook/localId.ts
@@ -0,0 +1,10 @@
+class localId {
+  private idMaps: new Map<string, string>
+  //tänne funktiot jotka generoi globalin id:n localille, ja globalista localin?
+  //jossain vaiheessa pitäis poistaakki sit ku orderiki poistettu?
+  public globalFromLocal async () => {
+  }
+  
+  public localFromGlobal async () => {
+  }
+}
