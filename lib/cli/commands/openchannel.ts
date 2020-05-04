a23d0584c25fa09b7aba84f512792ad0b4372b53
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-05-04 16:41:35 +0300

    Update openchannel.ts

--- lib/cli/commands/openchannel.ts
+++ lib/cli/commands/openchannel.ts
@@ -1,26 +1,17 @@
-import { Arguments } from 'yargs';
-import { callback, loadXudClient } from '../command';
-import { OpenChannelRequest } from '../../proto/xudrpc_pb';
-import { coinsToSats } from '../utils';
+.cmd(
+  name: 'Open Channel';
+  description: 'open a payment channel with a peer';
+  message: open_channel;
+  handler: ;
+)
 
-export const command = 'openchannel <node_identifier> <currency> <amount>';
+.cmd_descriptions(
+  node_id: 'the node key or alias of the connected peer to open the channel with';
+  currency: 'the ticker symbol for the currency';
+  amount: 'the amount to be deposited into the channel';
+)
 
-export const describe = 'open a payment channel with a peer';
 
-export const builder = {
-  node_identifier: {
-    description: 'the node key or alias of the connected peer to open the channel with',
-    type: 'string',
-  },
-  currency: {
-    description: 'the ticker symbol for the currency',
-    type: 'string',
-  },
-  amount: {
-    type: 'number',
-    description: 'the amount to be deposited into the channel',
-  },
-};
 
 export const handler = (argv: Arguments<any>) => {
   const request = new OpenChannelRequest();
