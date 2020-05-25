792d591b337fafeb085b88c7cac556ce2da87130
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-05-25 19:15:07 +0300

    Create swapClient.ts

---
+++ lib/swaps/simplified/swapClient.ts
@@ -0,0 +1,38 @@
+//Tässä kaikki funktiot joihin swap moduuli tai muut soittaa. Tääl koodit jotka yhteisiä kaikille swap clienteille,
+//mut sit swap cleinttien omissa foldereissa niille specifit koodit.
+
+class swapClient {
+  
+  
+  public waitForClientReady = (client){
+  }
+  
+  public init = async(){
+  }
+  
+  public getClientInfo = async(){
+  }
+  
+  public sendSmallestAmount = async(){
+  }
+  
+  public deposit = async(){
+  }
+  
+  public withdraw = async(){
+  }
+  
+  public openChannel = async(){
+  }
+  
+  public walletBalance = async(){
+  }
+  
+  public channelBalance = async(){
+  }
+  
+  public tradingLimits = async(){
+  }
+  
+  
+}
