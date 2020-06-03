03752ecbdb843a9b63e018344188b81a5d5e50e6
VETTE <46370333+xVETTEx@users.noreply.github.com>
2020-06-03 12:09:20 +0300

    Update ccxtRepository.ts

--- lib/ccxt/ccxtRepository.ts
+++ lib/ccxt/ccxtRepository.ts
@@ -1,1 +1,11 @@
 
+//tää soittaa db:n lisäks myös serviceen ja orderbookkiin.
+class ccxtRepository {
+  service;
+  orderbook;
+  constructor(service, orderbook){
+  }
+  public getBalance(){
+    return service.getBalance()
+  }
+}
